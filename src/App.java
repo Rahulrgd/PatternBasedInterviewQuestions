5860515e46d7b911c510e86aa3958ada686c73cf
RahulGupta <rahulrgd21@gmail.com>
2024-11-08 12:04:52 +0530

    Initial Commit

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        for(int i=1; i<=30; i++){
+            System.out.println("Question "+ i + ": ");
+        }
+    }
+}
